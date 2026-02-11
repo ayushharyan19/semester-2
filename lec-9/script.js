88cf2467fa592e66af35efa5881732c86abc434f
ayushharyan19 <ayush19062006@gmail.com>
2026-02-11 13:13:22 +0530

    updation

--- lec-9/script.js
+++ lec-9/script.js
@@ -1,9 +1,19 @@
-document.querySelector("#grandparent").addEventListener("click", ()=> {
-    console.log("Grandparent Clicked")
-})
-document.querySelector("#parent").addEventListener("click", ()=> {
-    console.log("Parent Clicked")
-})
-document.querySelector("#child").addEventListener("click", ()=> {
-    console.log("Child Clicked")
-})
+const container = document.getElementById("container");
+
+function printLog(message) {
+    const p = document.createElement("p");
+    p.textContent = message;
+    container.appendChild(p);
+}
+
+document.querySelector("#grandparent").addEventListener("click", () => {
+    printLog("Grandparent Clicked");
+});
+
+document.querySelector("#parent").addEventListener("click", () => {
+    printLog("Parent Clicked");
+});
+
+document.querySelector(".btn").addEventListener("click", () => {
+    printLog("Button Clicked");
+});
